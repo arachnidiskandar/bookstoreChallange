8cfdfc33b5112aeec6c1df493c538fa0c839c849
Augusto <augstarachrafael@gmail.com>
2020-08-21 18:21:32 -0300

    Models Creation

--- bookstore/apps/book/models.py
+++ bookstore/apps/book/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    class BookStatus(models.IntegerChoices):
+        AVAILABLE = 1
+        BORROWED = 2
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    status = models.IntegerField(choices=BookStatus.choices, default=1)
+    borrow_date = models.DateField(null=True, blank=True)
+    borrow_value = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.name
--- bookstore/apps/client/models.py
+++ bookstore/apps/client/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from ..book.models import Book
 
-# Create your models here.
+
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    borrowed_books = models.ManyToManyField(Book, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
